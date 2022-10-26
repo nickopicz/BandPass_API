50f22deba267509de458e551b99d7fd3621a74dd
Nick Ciraulo <nickopicz@gmail.com>
2022-10-26 01:05:26 -0400

    changed branch to start flask api development

---
+++ flask/Main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, flash, redirect, url_for
+from flask_ngrok import run_with_ngrok
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+run_with_ngrok(app)
+upload_dir = "../uploads"
+
+ALLOWED_EXTENSIONS = {'wav'}
+
+app.config['UPLOAD_FOLDER'] = upload_dir
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+if __name__ == "__main__":
+    app.run()
